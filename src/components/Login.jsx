9a19fece9ec78330c476b7f7e2ddd1529dd385bb
busanteam <busanteam_business@naver.com>
2022-11-07 05:40:18 +0900

    clean

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useSession } from '../hooks/session-context';
 
 // export const Login = ({ login }) => {
